1b9b07b839a056b401db20866d9afdfdbae0699d
Eldana63 <eladana973@gmail.com>
2025-12-22 06:58:10 -0800

    Remove CSS and implement toggle button functionality
    Removed CSS styles and added toggle functionality for text.

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-body{
-    margin: 0;
-    padding:0;
-    height: fit-content;
-    justify-content: center;
-    align-items: center;
-    background-color: azure;
-    font-family: Arial, Helvetica, sans-serif;
-}
-.container{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
-}
-h1{
-    text-align: center;
-    color: #333;
-}
-button{
-    padding: auto;
-    font-size: medium;
-    font-weight: 600;
-    cursor: pointer;
-    background-color: bisque;
-    color: white;
-    transition: background-color 0.2s ease;
-}
-button:hover{
-    background-color: #101923;
-}
+const toggleText=document.getElementById('toggleText');
+const toggleBtn=document.getElementById('togglrBtn');
+
+let isOn=true;
+
+toggleBtn.addEventListener('click',function(){
+    isOn=!isOn
+    toggleText.textContent=isOn?"ON":"OFF";
+});
