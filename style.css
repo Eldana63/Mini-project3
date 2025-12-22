9e6c880d4d3b23ef4816c89f638633ffa6a918e9
Eldana63 <eladana973@gmail.com>
2025-12-22 07:00:01 -0800

    Add initial CSS styles for body and buttons

---
+++ style.css
@@ -0,0 +1,31 @@
+body{
+    margin: 0;
+    padding:0;
+    height: fit-content;
+    justify-content: center;
+    align-items: center;
+    background-color: azure;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+}
+h1{
+    text-align: center;
+    color: #333;
+}
+button{
+    padding: auto;
+    font-size: medium;
+    font-weight: 600;
+    cursor: pointer;
+    background-color: bisque;
+    color: white;
+    transition: background-color 0.2s ease;
+}
+button:hover{
+    background-color: #101923;
+}
